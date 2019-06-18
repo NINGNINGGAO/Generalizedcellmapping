b50069613948ce5ac92b3733540ea09a518d4c35
NINGNINGGAO <707288044@qq.com>
2019-06-18 15:11:39 +0800

    delete code

--- code/K_Z.m
+++
@@ -1,8 +0,0 @@
-function SE=K_Z(C_SE,bcell)%SE为一个数，C_SE为一个胞序号行向量
-m=length(bcell);
-temp=1;
-SE=C_SE(:,1);
-for i=2:m
-    temp=temp*bcell(i-1);
-    SE=SE+(C_SE(:,i)-1)*temp;
-end
--- code/test.m
+++
@@ -1,5 +0,0 @@
-if isempty(SEC)==0
-SE_C=Z_K(SEC,bcell,cellnum);
-[~,SECR]=G(SE_C,interval,widght);
-scatter(SECR(:,1),SECR(:,2),1,'filled');
-end
