669a3f2417d73e63fa08234ba1234d77fb4b7965
NINGNINGGAO <707288044@qq.com>
2019-06-18 14:43:18 +0800

    note

--- CODE/Gabow.m
+++ CODE/Gabow.m
@@ -23,5 +23,11 @@ while any(Order==-1)
     Path=[];
     Root=[];
     [v,~]=find(Order==-1,1);
-    [temp,mid,Order,Part,Path,Root]=Gabowwolf(CMQ,m,v,temp,mid,Order,Part,Path,Root);
+    try
+        [temp,mid,Order,Part,Path,Root]=Gabowwolf(CMQ,m,v,temp,mid,Order,Part,Path,Root);
+    catch
+        warning('insufficient memory')
+        disp('Reducing the number of large or small cells')
+        disp('Or use computers with larger memory')
+        disp('Or if there is enough memory, change a larger value at set(0,RecursionLimit,1000)')
 end
